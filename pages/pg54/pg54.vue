6acb87c195328592b73a384734bb87750afd186f
ziyuanzhang <2661548928@qq.com>
2021-11-25 22:53:33 +0800

    d

---
+++ pages/pg54/pg54.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="A-page">
+		<web-view :webview-styles="webviewStyles" :src="src"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				src: "",
+				webviewStyles: {
+					progress: {
+						color: '#FF3333'
+					}
+				}
+			};
+		},
+		onLoad() {
+			let count = uni.getStorageSync('count');
+			count++;
+			uni.setStorageSync('count', count);
+			let pages = getCurrentPages();
+			let pageobj = pages[pages.length - 1];
+			let pageName = pageobj.$page.fullPath.split('/')[2];
+			this.src = `${this.$baseUrl}${pageName}/index.html?count=${count}`
+
+		},
+		methods: {}
+	};
+</script>
+
+<style lang="scss"></style>
